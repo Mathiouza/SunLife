824477d68342956bea9dc8b1551e275ca933cf2c
Mathieu Casubolo <mathieu.casubolo@hotmail.com>
2021-02-04 23:41:25 -0500

    Add navigation bar

--- index.php
+++ index.php
@@ -4,9 +4,38 @@
 		<meta charset="utf-8">
 		<title>SunLife</title>
 		<link rel="icon" href="./favicon.png">
-		<link href="<?php echo "style_"; echo $style_sheet_v; echo ".css"; ?>" rel="stylesheet" type="text/css">
+		<link href="./styles/styles.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
-		
+
+		<nav>
+			<div id="logo" >
+				<img src="./res/common/logo sunlife.png" />
+			</div>
+
+			<div id="navigation">
+				<div id="buttons" >
+					<div>Accueil</div>
+					<div>BD</div>
+					<div>Persos</div>
+					<div>Artworks</div>
+					<div>À propos</div>
+					<div>Plus</div>
+				</div>
+
+				<div id="border" ></div>
+			</div>
+		</nav>
+
+		<div id="summary" >
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec lectus placerat mi pellentesque efficitur eu id purus. 
+			Aliquam quis placerat diam. Quisque quis lacus iaculis, placerat dolor nec, elementum eros. Pellentesque habitant morbi tristique 
+			senectus et netus et malesuada fames ac turpis egestas. Nunc placerat non diam laoreet efficitur. In vulputate vestibulum dui, vel 
+			elementum mauris. Etiam risus est, tristique a turpis et, sollicitudin consectetur nunc. Sed commodo a elit sed lobortis. Vestibulum 
+			blandit dolor et vulputate malesuada. Aliquam eleifend orci elit, et rutrum metus commodo quis. Vestibulum non dolor vitae neque consectetur 
+			facilisis quis et tellus. Mauris vitae velit erat. Nam posuere a est in elementum. Quisque vestibulum imperdiet eleifend. Proin et molestie turpis,
+			eu rhoncus metus. Proin at ante convallis, suscipit metus non, venenatis leo.
+		</div>
+
 	</body>
 </html>
