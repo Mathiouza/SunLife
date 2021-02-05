81600fa4b707172b8001697c35758735ba5db2db
Mathieu Casubolo <mathieu.casubolo@hotmail.com>
2021-02-04 22:40:06 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>SunLife</title>
+		<link rel="icon" href="./favicon.png">
+		<link href="<?php echo "style_"; echo $style_sheet_v; echo ".css"; ?>" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		
+	</body>
+</html>
