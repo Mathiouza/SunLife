219e1f638a662c5fd8f19f892f6747adeec6ad0b
Mathiouza <mathiouza@live.fr>
2021-02-05 02:24:02 -0500

    Change .gitignore

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const fixBorderSize = () => {
+    let borders = document.getElementsByClassName('border');
+    for (let border of borders) {
+        if (border instanceof HTMLElement) {
+            border.style.height = '0';
+            setTimeout((border) => {
+                const height = border.previousElementSibling.clientHeight;
+                border.style.height = height + 'px';
+            }, 20, border);
+        }
+    }
+};
+window.onload = fixBorderSize;
+window.onresize = fixBorderSize;
