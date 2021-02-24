d30f37e09b6a9e49dc14886c1c0f5f412b4477d9
Mathiouza <mathiouza@live.fr>
2021-02-23 23:37:27 -0500

    Change meta

--- assets/meta.php
+++ assets/meta.php
@@ -1,8 +1,8 @@
-<meta name="Description" content="SunLife est un webcomic se déroulant dans un monde post guerre nucléaire avec des personnages anthropomorphiques. Suivez les aventures de Charlie Horcas, PDG de l'entreprise SunLife.">
-<meta property="og:title" content="SunLife le webcomic">
+<meta name="Description" content="SunLife is a comic taking place after a nuclear war. Follow Charlie Horcas, SunLife's CEO. By Mathieu Casubolo (Mathiouza).">
+<meta property="og:title" content="SunLife, the comic">
 <meta property="og:site_name" content="SunLife">
 <meta property="og:url" content="https://www.sunlifecomic.com">
-<meta property="og:description" content="SunLife est un webcomic se déroulant dans un monde post guerre nucléaire avec des personnages anthropomorphiques. Suivez les aventures de Charlie Horcas, PDG de l'entreprise SunLife.">
+<meta property="og:description" content="SunLife is a comic taking place after a nuclear war. Follow Charlie Horcas, SunLife's CEO. By Mathieu Casubolo (Mathiouza).">
 <meta property="og:type" content="website">
-<meta property="og:image" content="https://sunlifecomic.com/BD/fr/000.png" />
-<meta property="og:image:alt" content="SunLife cover" />
+<meta property="og:image" content="https://sunlifecomic.com/BD/meta.png" />
+<meta property="og:image:alt" content="Charlie Horcas" />
