1be6983c3498eaad75d38bc90756c484241532e5
Mathiouza <mathiouza@live.fr>
2021-02-24 00:49:16 -0500

    Add work in progress pages

---
+++ en/persos.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-166878957-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'UA-166878957-1');
+		</script>
+
+
+		<title>SunLife</title>
+		<link rel="icon" href="../favicon.png">
+		<link href="../styles/style.css" rel="stylesheet" type="text/css">
+        <link href="../styles/main-page.css" rel="stylesheet" type="text/css">
+		<meta http-equiv="Cache-control" content="no-cache">
+		<meta http-equiv="Expires" content="-1">
+
+		<?php include "../assets/meta.php" ?>
+
+		<script type="module" src="../js/main-page.js"></script>
+	</head>
+	<body>
+        <?php include "../assets/language.php" ?>
+
+		<?php include "../assets/nav-bar-en.php" ?>
+
+		<div style="text-align:center; font-size:xx-large;margin:100px;">Work in progress...</div>
+
+		<?php include "../assets/footer-en.php" ?>
+
+	</body>
+</html>
