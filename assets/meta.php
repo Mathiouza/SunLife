a71567308cfd23b0ab1d560dbd3d17c4e4381781
Mathiouza <mathiouza@live.fr>
2021-02-23 21:47:43 -0500

    Add metas

---
+++ assets/meta.php
@@ -0,0 +1,8 @@
+<meta name="Description" content="SunLife est un webcomic se déroulant dans un monde post guerre nucléaire avec des personnages anthropomorphiques. Suivez les aventures de Charlie Horcas, PDG de l'entreprise SunLife.">
+<meta property="og:title" content="SunLife le webcomic">
+<meta property="og:site_name" content="SunLife">
+<meta property="og:url" content="https://www.sunlifecomic.com">
+<meta property="og:description" content="SunLife est un webcomic se déroulant dans un monde post guerre nucléaire avec des personnages anthropomorphiques. Suivez les aventures de Charlie Horcas, PDG de l'entreprise SunLife.">
+<meta property="og:type" content="website">
+<meta property="og:image" content="https://sunlifecomic.com/BD/fr/000.png" />
+<meta property="og:image:alt" content="SunLife cover" />
